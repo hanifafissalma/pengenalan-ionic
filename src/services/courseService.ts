d33d821e5fe28dd5639a995245dcc70b4f8f6ff3
hanifafissalma.wartek <hanifa.fissalma@wartek.belajar.id>
2026-05-20 13:38:06 +0700

    adjust

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -1,7 +1,6 @@
 import axios from 'axios'
-const API_URL = 'https://jsonplaceholder.typicode.com'
 
 export const getCourses = async() => {
-    const res = await axios.get(`${API_URL}/posts?_limit=6`)
+    const res = await axios.get(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
     return res.data
 }
--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -14,12 +14,12 @@
 
       <ExploreContainer name="Tab 1 page" />
       <br/>
-      <a href="/camera">Ke Kamera</a>
+      <!-- <a href="/camera">Ke Kamera</a> -->
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+  import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+  import ExploreContainer from '@/components/ExploreContainer.vue';
 </script>
